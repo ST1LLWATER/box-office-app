56f86eceb69a1fd4f5e23c095ea9b6a58cc08b95
st1llwater <alokpunjofficial@gmail.com>
2021-09-16 14:52:48 +0530

    Navbar And Router Dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Nav from './components/Nav';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/starred" exact>
+          <Starred />
+        </Route>
+        <Route>
+          <center>This Is 404 Brick Not Found xD Lmao Ded...</center>
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Starred.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Starred() {
+  return (
+    <>
+      <h1>Starred</h1>
+      <p>This Is Starred</p>
+    </>
+  );
+}
+
+export default Starred;
