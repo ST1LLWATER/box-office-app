3c3b9c564cab7acd640619c3cd44539549c08384
st1llwater <alokpunjofficial@gmail.com>
2021-09-25 17:41:42 +0530

    Custom Hooks Management

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import { useReducer, useEffect } from 'react';
+import { useReducer, useEffect, useState } from 'react';
+import { apiGet } from './config';
 
 function showsReducer(state, action) {
   switch (action.type) {
@@ -29,6 +30,67 @@ function usePersistedReducer(reducer, initialState, key) {
 
 function useShows(key = 'shows') {
   return usePersistedReducer(showsReducer, [], key);
+}
+
+export function useLastQuery(key = 'lastQuery') {
+  const [input, setInput] = useState(() => {
+    const persisted = sessionStorage.getItem(key);
+
+    return persisted ? JSON.parse(persisted) : '';
+  });
+
+  const setpersistedInput = newState => {
+    setInput(newState);
+    sessionStorage.setItem(key, JSON.stringify(newState));
+  };
+
+  return [input, setpersistedInput];
+}
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_SUCCESS': {
+      return { isLoading: false, error: null, show: action.show };
+    }
+
+    case 'FETCH_FAILED': {
+      return { ...state, isLoading: false, error: action.error };
+    }
+    default:
+      return state;
+  }
+};
+
+export function useShow(showId) {
+  const [state, dispatch] = useReducer(reducer, {
+    show: null,
+    isLoading: true,
+    error: null,
+  });
+
+  useEffect(() => {
+    let isMounted = true;
+
+    apiGet(`/shows/${showId}?embed[]=seasons&embed[]=cast`)
+      .then(results => {
+        setTimeout(() => {
+          if (isMounted) {
+            dispatch({ type: 'FETCH_SUCCESS', show: results });
+          }
+        }, 2000);
+      })
+      .catch(err => {
+        if (isMounted) {
+          dispatch({ type: 'FETCH_SUCCESS', error: err.message });
+        }
+      });
+
+    return () => {
+      isMounted = false;
+    };
+  }, [showId]);
+
+  return state;
 }
 
 export default useShows;
--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,62 +1,16 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from 'react';
-import { useParams } from 'react-router';
-import Cast from '../components/show/Cast';
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import ShowMainData from '../components/show/ShowMainData';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
-import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
-import { InfoBlock, ShowPageWrapper } from './Show.styled';
+import Cast from '../components/show/Cast';
+import { ShowPageWrapper, InfoBlock } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'FETCH_SUCCESS': {
-      return { isLoading: false, error: null, show: action.show };
-    }
-
-    case 'FETCH_FAILED': {
-      return { ...state, isLoading: false, error: action.error };
-    }
-    default:
-      return state;
-  }
-};
-
-const initialState = {
-  show: null,
-  isLoading: true,
-  error: null,
-};
-
-function Show() {
+const Show = () => {
   const { id } = useParams();
-
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  useEffect(() => {
-    let isMounted = true;
-
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        setTimeout(() => {
-          if (isMounted) {
-            dispatch({ type: 'FETCH_SUCCESS', show: results });
-          }
-        }, 2000);
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', error: err.message });
-        }
-      });
-
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
+  const { show, isLoading, error } = useShow(id);
 
   if (isLoading) {
     return (
@@ -69,10 +23,8 @@ function Show() {
   }
 
   if (error) {
-    return <div>Error Occured: {error}</div>;
+    return <div>Error occured: {error}</div>;
   }
-
-  console.log(show._embedded);
 
   return (
     <ShowPageWrapper>
@@ -83,6 +35,7 @@ function Show() {
         summary={show.summary}
         tags={show.genres}
       />
+
       <InfoBlock>
         <h2>Details</h2>
         <Details
@@ -103,6 +56,6 @@ function Show() {
       </InfoBlock>
     </ShowPageWrapper>
   );
-}
+};
 
 export default Show;
